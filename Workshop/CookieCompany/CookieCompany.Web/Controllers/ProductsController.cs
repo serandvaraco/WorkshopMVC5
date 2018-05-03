961ff51623f168303b5b545466513a2306a7cf35
Sergio Vargas Acosta <serandvaraco@outlook.com>
2018-05-03 07:37:07 -0500

    Init Workshop

---
+++ Workshop/CookieCompany/CookieCompany.Web/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿/*
+    AUTOR:         SERGIO VARGAS 
+    DESCRIPTION:   Permite mantener las peticiones HTTP 
+                   y comunicación de vistas de los productos.  
+ 
+ */
+
+namespace CookieCompany.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class ProductsController : BaseController
+    {
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View(this.accessCatalog.GetProducts());
+        }
+
+
+        // TODO 1. Agregar las vistas requeridas para Agregar un nuevo Producto 
+
+        // TODO 2. Agregar las vistas  requeridas para Eliminar un producto 
+
+        // TODO 3. Agregar las vistas requeridas para Actualizar un producto 
+
+        // TODO 4. Agregar las vistas requeridas para ver el detalle de un producto
+    }
+}
