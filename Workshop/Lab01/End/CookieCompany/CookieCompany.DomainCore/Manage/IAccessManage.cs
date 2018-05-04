208fa44edf6f6bc9cc47a554e3fa59b8ab20fe13
SERGIO ANDRES VARGAS <serandvaraco@outlook.com>
2018-05-04 08:21:00 -0500

    Lab 01

---
+++ Workshop/Lab01/End/CookieCompany/CookieCompany.DomainCore/Manage/IAccessManage.cs
@@ -0,0 +1,22 @@
+﻿/*
+    AUTOR:         SERGIO VARGAS 
+    DESCRIPTION:   Pemrite la ejecución controlada por contratos
+ 
+ */
+namespace CookieCompany.DomainCore.Manage
+{
+    using CookieCompany.Model.Fluent;
+    using System.Collections.Generic;
+
+
+    public interface IAccessCatalog
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProduct(string code);
+        IEnumerable<Product> GetProducts(bool state);
+
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(string code);
+    }
+}
